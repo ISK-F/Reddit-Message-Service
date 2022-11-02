bf10c471b68ce7a574e3695585f65be22f0c5dbe
Vignesh Joglekar <rexvigasaurus@gmail.com>
2022-11-01 21:36:59 -0500

    Adds rate limit handling

--- pages/api/sendMessage.js
+++ pages/api/sendMessage.js
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
 				userAgent: 'LoanReminderBot/1.0.1 (http://reddit.com/u/SirWhoaWaitWhat)'
 			});
 
+			snoo.config({continueAfterRatelimitError: true});
+
 			const response = await snoo.composeMessage({
 				to: req.body.to,
 				subject: req.body.subject?.substring(0, 100),
